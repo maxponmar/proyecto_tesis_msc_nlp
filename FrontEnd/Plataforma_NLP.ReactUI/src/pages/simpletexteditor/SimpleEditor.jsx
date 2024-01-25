8f42101233ae4598e48b8ee4c7fee0234c678a72
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2024-01-24 20:00:29 -0700

    Add Freeling analysis functionality to SimpleEditor component

--- FrontEnd/Plataforma_NLP.ReactUI/src/pages/simpletexteditor/SimpleEditor.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/pages/simpletexteditor/SimpleEditor.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLazyGetFreelingResultsQuery } from '../../api/defaultApi';
 import AnalysisResults from './components/AnalysisResults';
 import AnalysisSelector from './components/AnalysisSelector';
 import { analyzeText, createWordDictionary } from './functions/processingText';
 
 function SimpleEditor() {
     const [result, setResult] = useState(null);
+    const [getFreelingAnalysis, freelingStatus] = useLazyGetFreelingResultsQuery();
 
     const [selectedOption, setSelectedOption] = useState({
         section: '',
@@ -20,6 +22,8 @@ function SimpleEditor() {
         setResult(highlightedText);
     };
 
+    useEffect(() => { console.log(freelingStatus) }, [freelingStatus]);
+
     return (
         <div className="flex h-[calc(100vh-210px)] m-10">
             <div className="flex flex-col flex-grow">
@@ -27,6 +31,10 @@ function SimpleEditor() {
                 <div className='flex-1 mt-2'>
                     {result ? <p>{result.highlightedText}</p> : null}
                 </div>
+                <button className='px-4 py-2 rounded bg-blue-400 text-black' onClick={() => {
+                    getFreelingAnalysis({ text: "hola esta es una prueba de freeling" });
+
+                }}>Freeling</button>
             </div>
             <div className="flex flex-col flex-basis[300px] flex-shrink-0 ml-5">
                 <AnalysisSelector selectedOption={selectedOption} setSelectedOption={setSelectedOption} />
