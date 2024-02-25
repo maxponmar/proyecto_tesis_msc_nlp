70802b834e25b75d47623d88839a6cae1503b7ec
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2024-02-25 14:56:37 -0700

    Format useNotification hook

--- FrontEnd/Plataforma_NLP.ReactUI/src/hooks/useNotification.js
+++ FrontEnd/Plataforma_NLP.ReactUI/src/hooks/useNotification.js
@@ -1,23 +1,27 @@
-import { useEffect } from 'react';
-import { toast } from 'react-hot-toast';
-import swal from 'sweetalert';
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+import swal from "sweetalert";
 
 export default function useNotification(
   status,
-  successMessage = 'Petición exitosa',
-  loadingMessage = 'Cargando...',
-  onConfirm = () => {},
+  successMessage = "Petición exitosa",
+  loadingMessage = "Cargando...",
+  onConfirm = () => {}
 ) {
   useEffect(() => {
     if (status.isSuccess) {
-      swal('Listo!', successMessage, 'success').then((confirm) => {
+      swal("Listo!", successMessage, "success").then((confirm) => {
         if (confirm) {
           onConfirm();
         }
       });
     }
     if (status.isError) {
-      swal('Error', status.error.data?.message ? status.error.data?.message : '', 'error').then((confirm) => {
+      swal(
+        "Error",
+        status.error.data?.message ? status.error.data?.message : "",
+        "error"
+      ).then((confirm) => {
         if (confirm) {
           onConfirm();
         }
