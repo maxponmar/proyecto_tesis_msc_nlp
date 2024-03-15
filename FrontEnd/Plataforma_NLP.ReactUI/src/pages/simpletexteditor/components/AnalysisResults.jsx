fa6b24f71185b83f6afefd93747a9f0b4ad78f6d
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-03-14 18:37:04 -0700

    formateo de codigo

--- FrontEnd/Plataforma_NLP.ReactUI/src/pages/simpletexteditor/components/AnalysisResults.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/pages/simpletexteditor/components/AnalysisResults.jsx
@@ -1,18 +1,37 @@
-import AnalysisScore from './AnalysisScore';
+import AnalysisScore from "./AnalysisScore";
 
-export default function AnalysisResults({ analyses, repeatedWords, commonWords, selectedOptionLimits }) {
+export default function AnalysisResults({
+  analyses,
+  repeatedWords,
+  commonWords,
+  selectedOptionLimits,
+}) {
   return (
     <div>
       <h2 className="text-3xl font-bold mb-6">Resultados</h2>
       <p>
-        Palabras Repetidas: <span className="font-bold text-xl">{repeatedWords}</span>
+        Palabras Repetidas:{" "}
+        <span className="font-bold text-xl">{repeatedWords}</span>
       </p>
       <p>
-        Palabras Comunes: <span className="font-bold text-xl">{commonWords}</span>
+        Palabras Comunes:{" "}
+        <span className="font-bold text-xl">{commonWords}</span>
       </p>
-      <AnalysisScore name={analyses[0].name} score={analyses[0].score} limits={selectedOptionLimits.variety} />
-      <AnalysisScore name={analyses[1].name} score={analyses[1].score} limits={selectedOptionLimits.density} />
-      <AnalysisScore name={analyses[2].name} score={analyses[2].score} limits={selectedOptionLimits.sophistication} />
+      <AnalysisScore
+        name={analyses[0].name}
+        score={analyses[0].score}
+        limits={selectedOptionLimits.variety}
+      />
+      <AnalysisScore
+        name={analyses[1].name}
+        score={analyses[1].score}
+        limits={selectedOptionLimits.density}
+      />
+      <AnalysisScore
+        name={analyses[2].name}
+        score={analyses[2].score}
+        limits={selectedOptionLimits.sophistication}
+      />
     </div>
   );
 }
