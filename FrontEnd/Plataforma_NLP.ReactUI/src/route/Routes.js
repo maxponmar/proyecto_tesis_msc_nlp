b0ab140aa0f275dd6637894dc7ce4c7ca3f1ed26
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-03-14 18:27:15 -0700

    Se renombran rutas

--- FrontEnd/Plataforma_NLP.ReactUI/src/route/Routes.js
+++ FrontEnd/Plataforma_NLP.ReactUI/src/route/Routes.js
@@ -1,84 +1,80 @@
-import { AiFillHome } from 'react-icons/ai';
-import { BiBookBookmark, BiLogIn, BiLogOut, BiPencil, BiSupport, BiTestTube } from 'react-icons/bi';
-import 'react-quill/dist/quill.snow.css';
-import FreelingApplication from '../pages/freeling/Freeling';
-import Home from '../pages/home/Home';
-import Login from '../pages/login/Login';
-import Unauthorized from '../pages/noauthorized/Unauthorized';
-import SimpleEditor from '../pages/simpletexteditor/SimpleEditor';
-import Tests from '../pages/simpletexteditor/Tests';
-import PdfGuide from '../pages/support/PdfGuide';
-import Support from '../pages/support/Support';
+import { AiFillHome } from "react-icons/ai";
+import {
+  BiBookBookmark,
+  BiLogIn,
+  BiLogOut,
+  BiPencil,
+  BiSupport,
+} from "react-icons/bi";
+import "react-quill/dist/quill.snow.css";
+import FreelingApplication from "../pages/freeling/Freeling";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import Unauthorized from "../pages/noauthorized/Unauthorized";
+import SimpleEditor from "../pages/simpletexteditor/SimpleEditor";
+import PdfGuide from "../pages/support/PdfGuide";
+import Support from "../pages/support/Support";
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: Home,
     isProtected: false,
     icon: AiFillHome,
-    title: 'Inicio',
-    allowedRoles: ['ALL'],
+    title: "Inicio",
+    allowedRoles: ["ALL"],
     showOnSidebar: true,
   },
   {
-    path: '/simpleeditor',
+    path: "/riquezalexica",
     component: SimpleEditor,
     isProtected: false,
     icon: BiPencil,
-    title: 'Text Editor',
-    allowedRoles: ['ALL'],
+    title: "Riqueza Lexica",
+    allowedRoles: ["ALL"],
     showOnSidebar: true,
   },
   {
-    path: '/freeling',
+    path: "/freeling",
     component: FreelingApplication,
     isProtected: false,
     icon: BiBookBookmark,
-    title: 'FreeLing',
-    allowedRoles: ['ALL'],
+    title: "FreeLing",
+    allowedRoles: ["ALL"],
     showOnSidebar: true,
   },
   {
-    path: '/tests',
-    component: Tests,
-    isProtected: false,
-    icon: BiTestTube,
-    title: 'Tests',
-    allowedRoles: ['ALL'],
-    showOnSidebar: true,
-  },
-  {
-    path: '/login',
+    path: "/login",
     component: Login,
     isProtected: false,
     icon: BiLogIn,
-    title: 'Login',
-    allowedRoles: ['ALL'],
+    title: "Login",
+    allowedRoles: ["ALL"],
     showOnSidebar: false,
   },
   {
-    path: '/guide',
+    path: "/guide",
     component: PdfGuide,
     isProtected: false,
     icon: BiLogOut,
-    title: 'Guía',
-    allowedRoles: ['ALL'],
+    title: "Guía",
+    allowedRoles: ["ALL"],
     showOnSidebar: false,
   },
   {
-    path: '/support',
+    path: "/support",
     component: Support,
     isProtected: false,
     icon: BiSupport,
-    title: 'Soporte',
-    allowedRoles: ['ALL'],
+    title: "Soporte",
+    allowedRoles: ["ALL"],
     showOnSidebar: false,
   },
   {
-    path: '/unauthorized',
+    path: "/unauthorized",
     component: Unauthorized,
     isProtected: false,
-    allowedRoles: ['ALL'],
+    allowedRoles: ["ALL"],
     showOnSidebar: false,
   },
 ];
