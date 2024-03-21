2593d234115e9993fba86067c6cb34db00c94bb6
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 10:15:03 -0700

    Se actualiza titulo e icono en header

--- FrontEnd/Plataforma_NLP.ReactUI/src/components/header/Header.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/components/header/Header.jsx
@@ -1,20 +1,22 @@
-import { Link } from 'react-router-dom';
-import ItnLogo from '../../assets/images/tecnm_itn.jpg';
-import config from '../../config/config.json';
+import { Link } from "react-router-dom";
+import config from "../../config/config.json";
 
-import { useAuth0 } from '@auth0/auth0-react';
-import Tooltip from '../tooltip/Tooltip';
+import { useAuth0 } from "@auth0/auth0-react";
+import Tooltip from "../tooltip/Tooltip";
 
 function Header() {
   const { user, isAuthenticated, logout } = useAuth0();
 
   return (
-    <header className="border-b border-black z-[1000] top-0 left-[60px] fixed w-full bg-white font-bold text-black text-center text-xl py-1 flex justify-between items-center h-[60px]">
-      <div className="px-5 w-full sm:w-[200px]  flex items-center justify-center">
-        <img className="translate-x-[-60px] sm:translate-x-0" src={ItnLogo} alt="Continental Logo" />
-      </div>
+    <header className=" z-[1000] top-0 left-[60px] fixed w-full bg-white font-bold text-black text-center text-xl py-1 flex justify-between items-center h-[60px]">
+      <Link
+        to="/"
+        className="translate-x-[-60px] sm:translate-x-0 px-5 flex items-center justify-center min-w-[380px] text-sm md:min-w-[400px] md:text-lg"
+      >
+        <img className="w-8 h-8" src="/retmeepro.svg" alt="Continental Logo" />
+        <p className="ml-1">{config.appHeader}</p>
+      </Link>
       <div className="w-full text-left px-10 hidden sm:block">
-        <Link to="/">{config.appHeader}</Link>
         {isAuthenticated ? (
           <div className="absolute top-0 right-10 mx-10 my-1 flex items-center justify-center">
             <p className="text-itn mr-1">{user.name}</p>
