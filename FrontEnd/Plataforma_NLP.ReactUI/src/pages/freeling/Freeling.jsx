01e5b0304c45ceba766d14dd2aa2f29c2056f283
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 11:00:10 -0700

    Se agrega boton de descarga para resultados de freeling

--- FrontEnd/Plataforma_NLP.ReactUI/src/pages/freeling/Freeling.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/pages/freeling/Freeling.jsx
@@ -20,6 +20,19 @@ const FreelingApplication = () => {
     getFreelingAnalysis({ text });
   };
 
+  const descargarArchivo = () => {
+    const contenido = freelingStatus.data?.result;
+    const blob = new Blob([contenido], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const enlaceDescarga = document.createElement("a");
+    enlaceDescarga.href = url;
+    enlaceDescarga.download = "freeling.txt";
+    document.body.appendChild(enlaceDescarga);
+    enlaceDescarga.click();
+    document.body.removeChild(enlaceDescarga);
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <div className="mx-auto">
       <form
@@ -80,6 +93,28 @@ const FreelingApplication = () => {
                 <p key={index}>{line}</p>
               ))}
             </div>
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 inline-flex justify-center items-center gap-1"
+              onClick={descargarArchivo}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </svg>
+              <p>Descargar resultados</p>
+            </button>
+
             <Wordgroups data={freelingStatus.data?.wordGroups} />
           </div>
         ) : null}
