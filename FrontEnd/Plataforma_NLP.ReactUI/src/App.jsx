5d6e13d2d75901d81dc6899505b7464d8c538105
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 10:15:56 -0700

    Se actualiza color de fondo

--- FrontEnd/Plataforma_NLP.ReactUI/src/App.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/App.jsx
@@ -1,10 +1,10 @@
-import { Toaster } from 'react-hot-toast';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Header from './components/header/Header';
-import Sidebar from './components/sidebar/Sidebar';
-import config from './config/config.json';
-import ProtectedRoute from './route/ProtectedRoute';
-import routes from './route/Routes';
+import { Toaster } from "react-hot-toast";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import config from "./config/config.json";
+import ProtectedRoute from "./route/ProtectedRoute";
+import routes from "./route/Routes";
 function App() {
   document.title = config.appTitle;
 
@@ -13,7 +13,7 @@ function App() {
       <div className="min-h-screen bg-stone-200">
         <Header />
         <Sidebar />
-        <div className="top-[60px] left-0 sm:left-[60px] absolute bg-stone-200 w-[100vw] sm:w-[calc(100vw-60px)] overflow-auto max-h-[calc(100vh-60px)]">
+        <div className="top-[60px] left-0 sm:left-[60px] absolute w-[100vw] sm:w-[calc(100vw-60px)] overflow-auto max-h-[calc(100vh-60px)]">
           <Routes>
             {routes.map((route, index) => (
               <Route
