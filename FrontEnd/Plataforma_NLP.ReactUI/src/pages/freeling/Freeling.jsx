4202cbc7cae9a8617cc4f66d197d09b1a3460567
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 11:41:27 -0700

    Se actualizan propiedades className y htmlFor correctamente

--- FrontEnd/Plataforma_NLP.ReactUI/src/pages/freeling/Freeling.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/pages/freeling/Freeling.jsx
@@ -95,7 +95,7 @@ const FreelingApplication = () => {
             </div>
             <button
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 inline-flex justify-center items-center gap-1"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 my-2 inline-flex justify-center items-center gap-1"
               onClick={descargarArchivo}
             >
               <svg
