69c60f4c7f87235c9325434ee3073636f0df2d08
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 10:15:16 -0700

    Se modifica home para mostrar solo componente de login

--- FrontEnd/Plataforma_NLP.ReactUI/src/pages/home/Home.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/pages/home/Home.jsx
@@ -1,29 +1,32 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { Link } from 'react-router-dom';
-import Login from '../login/Login';
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import Login from "../login/Login";
 
 function Home() {
   const { isAuthenticated, user } = useAuth0();
 
   return (
     <div className="animate-fadeIn flex flex-col items-center justify-center py-10 px-5 h-full">
-      <h2 className="text-2xl sm:text-3xl font-semibold text-center">
-        <span className="font-bold block">{isAuthenticated ? `Bienvenid@ ${user.name}` : 'Bienvenid@'}</span>
-      </h2>
-
       {!isAuthenticated ? (
+        <Login />
+      ) : (
         <>
-          <p className="text-xl font-bold">Inicie sesión para acceder a la plataforma</p>
-          <Login />
+          <h2 className="text-2xl sm:text-3xl font-semibold text-center">
+            <span className="font-bold block">
+              {isAuthenticated ? `Bienvenid@ ${user.name}` : "Bienvenid@"}
+            </span>
+          </h2>
+          <p className="text-xl font-bold">
+            Abra el menú de la izquiera para ver más opciones
+          </p>
+          <Link
+            to="/support"
+            className="bg-itn text-white font-bold px-5 py-2 rounded-md my-10 hover:scale-105 active:translate-y-1 text-xl sm:text-2xl"
+          >
+            Soporte
+          </Link>
         </>
-      ) : null}
-      <p className="text-xl font-bold">Abra el menú de la izquiera para ver más opciones</p>
-      <Link
-        to="/support"
-        className="bg-itn text-white font-bold px-5 py-2 rounded-md my-10 hover:scale-105 active:translate-y-1 text-xl sm:text-2xl"
-      >
-        Soporte
-      </Link>
+      )}
     </div>
   );
 }
