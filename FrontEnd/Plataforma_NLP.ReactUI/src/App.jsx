b6a4b11817082aced8c37bbe868ae6bdcbe4e00c
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 16:35:25 -0700

    Add AuthProvider to App.jsx

--- FrontEnd/Plataforma_NLP.ReactUI/src/App.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/App.jsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import config from "./config/config.json";
+import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./route/ProtectedRoute";
 import routes from "./route/Routes";
 function App() {
@@ -10,28 +11,30 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-stone-200">
-        <Header />
-        <Sidebar />
-        <div className="top-[60px] left-0 sm:left-[60px] absolute w-[100vw] sm:w-[calc(100vw-60px)] overflow-auto max-h-[calc(100vh-60px)]">
-          <Routes>
-            {routes.map((route, index) => (
-              <Route
-                key={index}
-                path={route.path}
-                element={
-                  <ProtectedRoute
-                    isProtected={route.isProtected}
-                    allowedRoles={route.allowedRoles}
-                    element={<route.component />}
-                  />
-                }
-              />
-            ))}
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
+      <AuthProvider>
+        <div className="min-h-screen bg-stone-200">
+          <Header />
+          <Sidebar />
+          <div className="top-[60px] left-0 sm:left-[60px] absolute w-[100vw] sm:w-[calc(100vw-60px)] overflow-auto max-h-[calc(100vh-60px)]">
+            <Routes>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={
+                    <ProtectedRoute
+                      isProtected={route.isProtected}
+                      allowedRoles={route.allowedRoles}
+                      element={<route.component />}
+                    />
+                  }
+                />
+              ))}
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </AuthProvider>
       <Toaster />
     </>
   );
