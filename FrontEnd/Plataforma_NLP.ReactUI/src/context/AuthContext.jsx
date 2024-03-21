0e8b51340ebfd84845f629ed31f197f48c56e30e
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 16:35:21 -0700

    Add AuthContext component

---
+++ FrontEnd/Plataforma_NLP.ReactUI/src/context/AuthContext.jsx
@@ -0,0 +1,111 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { account } from "../appwrite/appwriteConfig";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const loginUser = async (userInfo) => {
+    setLoading(true);
+    try {
+      let response = await account.createEmailPasswordSession(
+        userInfo.email,
+        userInfo.password
+      );
+
+      let accountDetails = await account.get();
+
+      console.log(accountDetails);
+
+      setUser(accountDetails);
+    } catch (error) {
+      if (error?.type === "user_invalid_credentials") {
+        toast.error(
+          "Credenciales no validas, verifique su usuario y contraseña"
+        );
+      }
+
+      if (error?.type === "general_argument_invalid") {
+        toast.error("Verifique su usuario y contraseña");
+      }
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  const logoutUser = async () => {
+    setLoading(true);
+    try {
+      await account.deleteSession("current");
+      setUser(null);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  const registerUser = async (userInfo) => {};
+
+  const checkUserStatus = async () => {
+    try {
+      let accountDetails = await account.get();
+      setUser(accountDetails);
+      console.log(accountDetails);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  const contextDate = {
+    user,
+    loginUser,
+    logoutUser,
+    registerUser,
+    checkUserStatus,
+  };
+
+  useEffect(() => {
+    checkUserStatus();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={contextDate}>
+      {loading ? (
+        <div
+          role="status"
+          className="h-screen w-screen flex items-center justify-center"
+        >
+          <svg
+            aria-hidden="true"
+            className="w-16 h-16 text-gray-200 animate-spin fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Cargando...</span>
+        </div>
+      ) : (
+        children
+      )}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default AuthContext;
