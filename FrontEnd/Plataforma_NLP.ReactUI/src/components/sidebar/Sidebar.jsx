f0931d22b6d1800d1d4e15d7eb716b3e7dae1b03
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 10:18:17 -0700

    Se actualiza icono en sidebar

--- FrontEnd/Plataforma_NLP.ReactUI/src/components/sidebar/Sidebar.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,9 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { useState } from 'react';
-import { BiHelpCircle, BiLogIn, BiLogOut, BiSupport } from 'react-icons/bi';
-import { FaBars } from 'react-icons/fa';
-import ItnLogo from '../../assets/images/itn.ico';
-import routes from '../../route/Routes';
-import SidebarLink from '../../route/SidebarLink';
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState } from "react";
+import { BiHelpCircle, BiLogIn, BiLogOut, BiSupport } from "react-icons/bi";
+import { FaBars } from "react-icons/fa";
+import routes from "../../route/Routes";
+import SidebarLink from "../../route/SidebarLink";
 
 function Sidebar() {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -21,16 +20,26 @@ function Sidebar() {
           <FaBars />
         </div>
       )}
-      <div className={`absolute flex h-[100vh] ${showSidebar ? 'w-full' : 'w-0'}`}>
+      <div
+        className={`absolute flex h-[100vh] ${showSidebar ? "w-full" : "w-0"}`}
+      >
         <div
           className={`text-white pb-5 flex flex-col bg-clip-padding backdrop-filter backdrop-blur-md border border-black bg-sidebarcolor/[0.9] w-[250px] z-[1500] ease-in-out duration-300 ${
-            showSidebar ? 'translate-x-0 ' : 'sm:-translate-x-[calc(100%-60px)] -translate-x-full'
+            showSidebar
+              ? "translate-x-0 "
+              : "sm:-translate-x-[calc(100%-60px)] -translate-x-full"
           }`}
           onMouseEnter={() => setShowSidebar(true)}
           onMouseLeave={() => setShowSidebar(false)}
         >
           <div className="bg-black h-[64px] flex items-center justify-center">
-            {showSidebar && <img className="h-[55px] " src={ItnLogo} alt="Continental Logo" />}
+            {showSidebar && (
+              <img
+                className="h-[55px] p-2"
+                src="/retmeepro.svg"
+                alt="Continental Logo"
+              />
+            )}
           </div>
           <div className="flex flex-col justify-between	h-full">
             <div>
@@ -39,8 +48,13 @@ function Sidebar() {
                 .map((route, index) => {
                   if (
                     !route.isProtected ||
-                    (isAuthenticated && route.isProtected && route.allowedRoles?.includes('ALL')) ||
-                    (route.isProtected && route.allowedRoles.some((role) => user?.name.includes(role)))
+                    (isAuthenticated &&
+                      route.isProtected &&
+                      route.allowedRoles?.includes("ALL")) ||
+                    (route.isProtected &&
+                      route.allowedRoles.some((role) =>
+                        user?.name.includes(role)
+                      ))
                   )
                     return (
                       <SidebarLink
@@ -55,17 +69,40 @@ function Sidebar() {
             </div>
             <div>
               {isAuthenticated ? (
-                <SidebarLink title="Logout" link="/" icon={<BiLogOut />} onClick={() => logout()} />
+                <SidebarLink
+                  title="Logout"
+                  link="/"
+                  icon={<BiLogOut />}
+                  onClick={() => logout()}
+                />
               ) : (
-                <SidebarLink title="Login" link="/login" icon={<BiLogIn />} onClick={() => setShowSidebar(false)} />
+                <SidebarLink
+                  title="Login"
+                  link="/login"
+                  icon={<BiLogIn />}
+                  onClick={() => setShowSidebar(false)}
+                />
               )}
-              <SidebarLink title="Guía" link="/guide" icon={<BiHelpCircle />} onClick={() => setShowSidebar(false)} />
-              <SidebarLink title="Soporte" link="/support" icon={<BiSupport />} onClick={() => setShowSidebar(false)} />
+              <SidebarLink
+                title="Guía"
+                link="/guide"
+                icon={<BiHelpCircle />}
+                onClick={() => setShowSidebar(false)}
+              />
+              <SidebarLink
+                title="Soporte"
+                link="/support"
+                icon={<BiSupport />}
+                onClick={() => setShowSidebar(false)}
+              />
             </div>
           </div>
         </div>
         {showSidebar && (
-          <div className={`h-full bg-transparent  w-[calc(100%-200px)]`} onClick={() => setShowSidebar(false)}></div>
+          <div
+            className={`h-full bg-transparent  w-[calc(100%-200px)]`}
+            onClick={() => setShowSidebar(false)}
+          ></div>
         )}
       </div>
     </>
