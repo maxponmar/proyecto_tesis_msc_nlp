8b977289b901ca3ee81f7288f93f826ea18c0a06
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2024-03-21 11:40:50 -0700

    Se actualizan propiedades className y htmlFor correctamente

--- FrontEnd/Plataforma_NLP.ReactUI/src/pages/login/Login.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/pages/login/Login.jsx
@@ -3,26 +3,26 @@ import { useAuth0 } from "@auth0/auth0-react";
 function Login() {
   const { loginWithPopup, isAuthenticated } = useAuth0();
   return (
-    <section class="bg-stone-200 relative flex flex-col justify-center items-center">
+    <section className="bg-stone-200 relative flex flex-col justify-center items-center">
       {isAuthenticated ? <Navigate to="/" /> : null}
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
         <a
           href="#"
-          class="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
         >
-          <img class="w-8 h-8 mr-2" src="/retmeepro.svg" alt="logo" />
+          <img className="w-8 h-8 mr-2" src="/retmeepro.svg" alt="logo" />
           RetmeePro
         </a>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Iniciar sesión
             </h1>
-            <form class="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" action="#">
               <div>
                 <label
-                  for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900"
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Correo electrónico
                 </label>
@@ -30,15 +30,15 @@ function Login() {
                   type="email"
                   name="email"
                   id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="nombre@email.com"
                   required=""
                 />
               </div>
               <div>
                 <label
-                  for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900"
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Contraseña
                 </label>
@@ -47,22 +47,22 @@ function Login() {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   required=""
                 />
               </div>
               <button
                 type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
               >
                 Iniciar Sesión
               </button>
 
-              <p class="text-sm font-light text-gray-500">
+              <p className="text-sm font-light text-gray-500">
                 Aún no tiene una cuenta?{" "}
                 <a
                   href="#"
-                  class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Registrese aquí
                 </a>
