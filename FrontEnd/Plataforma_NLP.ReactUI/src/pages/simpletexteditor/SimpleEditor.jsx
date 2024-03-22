380fb6d78ab0625ebd8d349b04f19ee9dec5fd52
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-03-21 18:03:49 -0700

    Add an "Analyzing" indicator to the user

--- FrontEnd/Plataforma_NLP.ReactUI/src/pages/simpletexteditor/SimpleEditor.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/pages/simpletexteditor/SimpleEditor.jsx
@@ -12,6 +12,7 @@ import {
 
 function SimpleEditor() {
   const [userInput, setUserInput] = useState("");
+  const [analyzing, setAnalyzing] = useState(false);
   const [debouncedInput] = useDebounce(userInput, 1000);
 
   const [textToAnalyze, setTextToAnalyze] = useState("");
@@ -118,6 +119,10 @@ function SimpleEditor() {
   };
 
   useEffect(() => {
+    setAnalyzing(true);
+  }, [userInput]);
+
+  useEffect(() => {
     if (debouncedInput.length === 0 || selectedOption.section.length === 0)
       return;
 
@@ -159,9 +164,12 @@ function SimpleEditor() {
       let processedDictionary = procesarDiccionario(diccionario);
       setWordDictionary(processedDictionary);
     });
+    console.log("Texto para analizar: ", textToAnalyze);
   }, [textToAnalyze]);
 
   useEffect(() => {
+    setAnalyzing(false);
+
     if (Object.keys(wordDictionary).length === 0 || textToAnalyze.length === 0)
       return;
 
@@ -187,6 +195,27 @@ function SimpleEditor() {
             }}
             className="flex-1 resize-none"
           />
+          <button
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 rounded-full text-sm px-2 py-1 text-center mb-2 inline-flex items-center justify-center gap-2 max-w-48"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+              />
+            </svg>
+            Guardar avances
+          </button>
+
           <div className="flex-1 mt-2">
             {result ? <p>{result.highlightedText}</p> : null}
           </div>
@@ -218,7 +247,27 @@ function SimpleEditor() {
           selectedOption={selectedOption}
           setSelectedOption={setSelectedOption}
         />
-        {result && selectedOption.section ? (
+        {analyzing ? (
+          <div role="status" className="flex items-center justify-center">
+            <svg
+              aria-hidden="true"
+              className="w-16 h-16 text-gray-200 animate-spin fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Cargando...</span>
+          </div>
+        ) : result && selectedOption.section && !analyzing ? (
           <AnalysisResults
             analyses={result.scores}
             commonWords={result.commonWords}
@@ -228,6 +277,7 @@ function SimpleEditor() {
         ) : (
           <p>Seleccione una opción para ver los resultados</p>
         )}
+        {}
       </div>
     </div>
   );
