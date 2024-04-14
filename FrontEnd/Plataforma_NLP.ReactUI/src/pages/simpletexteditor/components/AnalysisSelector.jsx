e30e3f0869b5bea2b414f9d641eaad59af84f0e3
Maximiliano Ponce <maxponce.marquez@outlook.com>
2024-04-14 09:12:07 -0700

    Correccion gramatical

--- FrontEnd/Plataforma_NLP.ReactUI/src/pages/simpletexteditor/components/AnalysisSelector.jsx
+++ FrontEnd/Plataforma_NLP.ReactUI/src/pages/simpletexteditor/components/AnalysisSelector.jsx
@@ -22,7 +22,7 @@ export default function AnalysisSelector({
           onChange={(e) => setTitle(e.target.value)}
         />
         <label className="peer-focus:font-medium absolute text-md duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-          Titulo del documento
+          Título del documento
         </label>
       </div>
       <p className="my-4">Por favor, elija que evaluará:</p>
